c6a241c4a385bb3c8251067a2dc31205e5967dc6
nandev04 <renanlvsdv@gmail.com>
2025-09-15 12:23:30 -0300

    feat: erro da userService tratado

--- backend/src/services/userService.ts
+++ backend/src/services/userService.ts
@@ -2,32 +2,51 @@ import { usersType } from '../types/users.js';
 import * as Model from '../models/userModel.js';
 import bcrypt from 'bcrypt';
 import { AuthService } from './authService.js';
+import { AppError } from '../utils/error.js';
 
 const getUser = async (id: string) => {
-  const user = await Model.getUser(+id);
-  return user;
+  try {
+    const user = await Model.getUser(+id);
+    return user;
+  } catch (err) {
+    throw err;
+  }
 };
 
 const createUser = async ({ username, password, email }: usersType) => {
-  // Preciso tratar os erros
-  const hashPassword = await bcrypt.hash(password, 10);
+  try {
+    // Preciso tratar os erros
+    const hashPassword = await bcrypt.hash(password, 10);
 
-  const createdUser = await Model.createUser({ username, hashPassword, email });
+    const createdUser = await Model.createUser({ username, hashPassword, email });
 
-  const token = await AuthService.register(createdUser.id, createdUser.email);
+    const token = await AuthService.register(createdUser.id, createdUser.email);
 
-  return createdUser;
+    return createdUser;
+  } catch (err) {
+    if (err instanceof AppError) throw err;
+
+    throw new AppError(err instanceof Error ? err.message : 'Erro Desconhecido', 500);
+  }
 };
 
 const editUser = async ({ id, data }: { id: string; data: {} }) => {
-  const editedUser = await Model.editUser({ id: Number(id), data });
+  try {
+    const editedUser = await Model.editUser({ id: Number(id), data });
 
-  return editedUser;
+    return editedUser;
+  } catch (err) {
+    throw err;
+  }
 };
 
 const deleteUser = async (id: string) => {
-  const deletedUser = await Model.deleteUser(+id);
-  return deletedUser;
+  try {
+    const deletedUser = await Model.deleteUser(+id);
+    return deletedUser;
+  } catch (err) {
+    throw err;
+  }
 };
 
 export { getUser, createUser, editUser, deleteUser };
