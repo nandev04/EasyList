37946df62dc680be3ab4d1b7aa3cc415094cf400
nandev04 <renanlvsdv@gmail.com>
2025-12-15 15:38:01 -0300

    refactor: modulo de user e task refatorado

--- backend/src/modules/task/task.schema.ts
+++ backend/src/modules/task/task.schema.ts
@@ -1,7 +1,19 @@
 import { z } from 'zod';
 
+type CreateTaskSchemaType = z.infer<typeof createTaskSchema>;
+type deleteTaskSchemaParamsType = z.infer<typeof deleteTaskSchemaParams>;
 type updateTaskSchemaBodyType = z.infer<typeof updateTaskSchemaBody>;
 type updateTaskSchemaParamsType = z.infer<typeof updateTaskSchemaParams>;
+
+const createTaskSchema = z.object({
+  title: z.string().min(1).max(255),
+  description: z.string().trim().min(1).optional().nullable(),
+  status: z.enum(['PENDING', 'COMPLETED', 'IN_PROGRESS']).default('PENDING')
+});
+
+const deleteTaskSchemaParams = z.object({
+  id: z.coerce.number().positive().int()
+});
 
 const updateTaskSchemaParams = z.object({
   id: z.coerce.number().positive().int()
@@ -14,6 +26,10 @@ const updateTaskSchemaBody = z.object({
 });
 
 export {
+  createTaskSchema,
+  CreateTaskSchemaType,
+  deleteTaskSchemaParams,
+  deleteTaskSchemaParamsType,
   updateTaskSchemaBody,
   updateTaskSchemaBodyType,
   updateTaskSchemaParams,
