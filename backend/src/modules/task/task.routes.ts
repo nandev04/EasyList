ecdfd6f68acf3aa52d5ab18098c24f9d110fec11
Renan Alves <renanlvsdv@gmail.com>
2026-01-03 09:11:18 -0300

    feature: finalizado rateLimit para rotas do modulo task

--- backend/src/modules/task/task.routes.ts
+++ backend/src/modules/task/task.routes.ts
@@ -3,24 +3,28 @@ import * as taskController from './task.controller.js';
 import validate from '../../middlewares/validateData.js';
 import { createTaskSchema, updateTaskSchemaBody, deleteTaskSchemaParams } from './task.schema.js';
 import { validateJwt } from '../../middlewares/validateJwt.js';
+import * as Rate_Limit from '../../middlewares/rateLimit.js';
 
 const taskRoutes = express.Router();
 
-taskRoutes.get('/tasks', validateJwt, taskController.getTasks);
+taskRoutes.get('/tasks', Rate_Limit.getTasks, validateJwt, taskController.getTasks);
 taskRoutes.post(
   '/tasks',
+  Rate_Limit.createtask,
   validateJwt,
   validate({ body: createTaskSchema }),
   taskController.createTask
 );
 taskRoutes.patch(
   '/tasks/:id',
+  Rate_Limit.editTask,
   validateJwt,
   validate({ body: updateTaskSchemaBody, params: deleteTaskSchemaParams }),
   taskController.updateTask
 );
 taskRoutes.delete(
   '/tasks/:id',
+  Rate_Limit.deleteTask,
   validateJwt,
   validate({ params: deleteTaskSchemaParams }),
   taskController.removeTask
