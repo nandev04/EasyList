a7b7969099183db28d0710559b61129f9dd28376
Renan Alves <renanlvsdv@gmail.com>
2026-01-04 11:25:43 -0300

    feat: adicionado middleware para validar upload de avatar

--- backend/src/modules/user/users.routes.ts
+++ backend/src/modules/user/users.routes.ts
@@ -5,8 +5,11 @@ import { createUserBodySchema } from './user.schema.js';
 import { updateUserSchemaBody } from './user.schema.js';
 import { validateJwt } from '../../middlewares/validateJwt.js';
 import * as Rate_Limit from '../../middlewares/rateLimit.js';
+import * as Validate_Avatar from '../../middlewares/validateFile.js';
 
 const userRoutes = express.Router();
+
+// Criar rota de getUser
 
 userRoutes.post(
   '/user',
@@ -14,6 +17,7 @@ userRoutes.post(
   validate({ body: createUserBodySchema }),
   Controller.createUser
 );
+
 userRoutes.patch(
   '/user/',
   Rate_Limit.editUser,
@@ -21,6 +25,14 @@ userRoutes.patch(
   validate({ body: updateUserSchemaBody }),
   Controller.updateUser
 );
+
 userRoutes.delete('/user', Rate_Limit.deleteUser, validateJwt, Controller.deleteUser);
+
+userRoutes.post(
+  '/avatar/upload',
+  validateJwt,
+  Validate_Avatar.uploadAvatar.single('avatar'),
+  Controller.uploadAvatar
+);
 
 export default userRoutes;
