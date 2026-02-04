44c8ae0fc6593734f795b70d1367d971c3aaca48
Renan Alves <renanlvsdv@gmail.com>
2026-02-04 06:11:27 -0300

    feat: finalizado sistema de login e navigate, melhorias a serem feitas

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,10 @@
-import { createRoot } from 'react-dom/client';
-import './styles/global.css';
-import { BrowserRouter } from 'react-router-dom';
-import MainRoutes from './routes';
+import { createRoot } from "react-dom/client";
+import "./styles/global.css";
+import { BrowserRouter } from "react-router-dom";
+import MainRoutes from "./routes";
 
-createRoot(document.getElementById('root')!).render(
-	<BrowserRouter>
-		<MainRoutes />
-	</BrowserRouter>,
+createRoot(document.getElementById("root")!).render(
+  <BrowserRouter>
+    <MainRoutes />
+  </BrowserRouter>,
 );
