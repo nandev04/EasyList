984535923410e7549ed3d841b4d26e934f4a89e0
Renan Alves <renanlvsdv@gmail.com>
2026-02-12 06:00:43 -0300

    feat: loadingCreateTask feito, implementado delay de loading

--- frontend/src/components/createTaskBtn/CreateTaskBtn.tsx
+++ frontend/src/components/createTaskBtn/CreateTaskBtn.tsx
@@ -6,23 +6,35 @@ import { IoCloseSharp } from "react-icons/io5";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { taskSchema, taskSchemaType } from "../../schemas/taskSchema";
-
+import LoadingCircleSpinner from "../loadingCircleSpinner/loadingCircleSpinner";
+import { createTask } from "../../services/task.service";
+import { queryClient } from "../../lib/reactQuery";
+import useDelayLoading from "../../hooks/useDelayLoading";
 const CreateTaskBtn = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const [loadingCreateTask, setLoadingCreateTask] = useState(false);
   const {
     register,
     handleSubmit,
     formState: { errors },
+    reset,
   } = useForm<taskSchemaType>({
     resolver: zodResolver(taskSchema),
     mode: "onSubmit",
   });
+  const { showLoading } = useDelayLoading(loadingCreateTask, 150);
 
-  function envia(data: taskSchemaType) {
+  async function onSubmit(data: taskSchemaType): Promise<void> {
     try {
-      console.log(data);
+      setLoadingCreateTask(true);
+      await createTask(data);
+      queryClient.invalidateQueries({ queryKey: ["tasks"] });
+      reset();
+      setIsOpen(false);
     } catch (err) {
       console.log(err);
+    } finally {
+      setLoadingCreateTask(false);
     }
   }
 
@@ -35,14 +47,16 @@ const CreateTaskBtn = () => {
       </div>
       <Dialog
         open={isOpen}
-        onClose={() => setIsOpen(false)}
+        onClose={() => {
+          (reset(), setIsOpen(false));
+        }}
         className={style.root}
       >
         <div className={style.overlay} />
         <div className={style.container}>
           <DialogPanel className={style.panel}>
             <div className={style.panel_content}>
-              <form onSubmit={handleSubmit(envia)}>
+              <form onSubmit={handleSubmit(onSubmit)}>
                 <input
                   type="text"
                   className={style.title_input}
@@ -66,14 +80,20 @@ const CreateTaskBtn = () => {
                 )}
 
                 <div className={style.actions}>
-                  <button type="submit" className={style.createTask}>
-                    Criar Tarefa
+                  <button
+                    disabled={loadingCreateTask}
+                    type="submit"
+                    className={style.createTask}
+                  >
+                    {showLoading ? <LoadingCircleSpinner /> : "Criar tarefa"}
                   </button>
                 </div>
               </form>
               <button
                 className={style.button_close}
-                onClick={() => setIsOpen(false)}
+                onClick={() => {
+                  (reset(), setIsOpen(false));
+                }}
               >
                 <IoCloseSharp />
               </button>
