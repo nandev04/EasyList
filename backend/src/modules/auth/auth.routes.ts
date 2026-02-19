f75e1ce21eb6726fa732c4c6c4aa2b1d7dd609b5
Renan Alves <renanlvsdv@gmail.com>
2026-02-19 01:56:17 -0300

    fix: bug de deviceId não sendo adicionado aos cookies em caso de existência de accessToken, resolvido

--- backend/src/modules/auth/auth.routes.ts
+++ backend/src/modules/auth/auth.routes.ts
@@ -14,6 +14,7 @@ import * as Controller_Login from './login.controller.js';
 import { authenticate } from '../../middlewares/authenticate.js';
 import * as Rate_Limit from '../../middlewares/rateLimit.js';
 import resolveSessionIfExists from '../../middlewares/resolveSessionIfExists.js';
+import requireAuth from '../../middlewares/requireAuth.js';
 
 const authRoutes = express.Router();
 
