dd3b3e21c420598aa8926549a6f16e25fa34b32a
Renan Alves <renanlvsdv@gmail.com>
2026-03-11 15:35:48 -0300

    feat: criado função utilitária  para mascarar email e seu respectivo teste unitário

---
+++ backend/src/shared/utils/emailMask.spec.ts
@@ -0,0 +1,29 @@
+import emailMask from './emailMask';
+
+describe('Mask email', () => {
+  test('Should return mask email, showing 3 local part characters and domain', async () => {
+    const emailTest = 'exemplo.teste@gmail.com';
+    const emailTest2 = 'exem@gmail.com';
+    const expectReturn = 'exe**********@gmail.com';
+    const expectReturn2 = 'exe*@gmail.com';
+
+    const emailMasked = emailMask(emailTest);
+    const emailMasked2 = emailMask(emailTest2);
+
+    expect(emailMasked).toEqual(expectReturn);
+    expect(emailMasked2).toEqual(expectReturn2);
+  });
+
+  test('Should return mask email, hiding everything because the local part is less than or equal 3', async () => {
+    const emailTestTwoCharacters = 'ex@gmail.com';
+    const emailTestThreeCharacters = 'exe@gmail.com';
+    const expectReturnTwo = '**@gmail.com';
+    const expectReturnThreeCharacters = '***@gmail.com';
+
+    const emailMaskedTwo = emailMask(expectReturnTwo);
+    const emailMaskedThree = emailMask(expectReturnThreeCharacters);
+
+    expect(emailMaskedTwo).toEqual(expectReturnTwo);
+    expect(emailMaskedThree).toEqual(expectReturnThreeCharacters);
+  });
+});
