a6f614e9b74c8344f3f408fa8a3e7247ea14613f
Renan Alves <renanlvsdv@gmail.com>
2026-04-23 01:55:38 -0300

    refactor: design da página de login refatorada

--- frontend/src/pages/login/index.tsx
+++ frontend/src/pages/login/index.tsx
@@ -2,6 +2,7 @@ import { Navigate } from "react-router-dom";
 import LoginForm from "../../components/loginForm/loginForm";
 import styles from "./login.module.css";
 import { useGetUser } from "../../hooks/React/useUser";
+import { IoLogIn } from "react-icons/io5";
 
 const Login = () => {
   const { data: user, isLoading } = useGetUser();
@@ -11,10 +12,13 @@ const Login = () => {
   if (user) return <Navigate to="/" />;
   return (
     <main className={styles.main_login}>
-      <div className={styles.container_main}>
-        <div className={styles.container_input}>
-          <LoginForm />
+      <div className={styles.left_panel}>
+        <div className={styles.icon_wrapper}>
+          <IoLogIn />
         </div>
+      </div>
+      <div className={styles.right_panel}>
+        <LoginForm />
       </div>
     </main>
   );
