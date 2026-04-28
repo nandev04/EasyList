e3fb7811bcfc6a76696b1f3591f905bb5cede005
Renan Alves <renanlvsdv@gmail.com>
2026-04-28 01:06:53 -0300

    feat: implementado 3/4 componente do fluxo de forgotPassword;

--- frontend/src/pages/forgot-password/index.tsx
+++ frontend/src/pages/forgot-password/index.tsx
@@ -1,9 +1,13 @@
+import { useState } from "react";
 import styles from "./forgotPassword.module.css";
 import ComponentEmailInput from "../../components/componentEmailInput/ComponentEmailInput";
 import ComponentOtp from "../../components/componentOtp/ComponentOtp";
+import ComponentResetPassword from "../../components/componentResetPassword/ComponentResetPassword";
+
+type Step = "email" | "otp" | "reset";
 
 const ForgotPassword = () => {
-  const showOtp = true;
+  const [step, setStep] = useState<Step>("reset");
 
   return (
     <>
@@ -12,7 +16,9 @@ const ForgotPassword = () => {
       </span>
       <div className={styles.base}>
         <div className={styles.general_container}>
-          {showOtp ? <ComponentOtp /> : <ComponentEmailInput />}
+          {step === "email" && <ComponentEmailInput />}
+          {step === "otp" && <ComponentOtp />}
+          {step === "reset" && <ComponentResetPassword />}
         </div>
       </div>
     </>
