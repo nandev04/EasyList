1be9bf56117195d2b3eceb3f7c787174c6ae2f76
Renan Alves <renanlvsdv@gmail.com>
2026-05-05 14:44:46 -0300

    chore: removido endpoint de refreshToken
    - responsabilidade atribuída a middlewares

--- backend/src/modules/auth/use-cases/refreshToken/refreshTkn.schema.ts
+++
@@ -1,8 +0,0 @@
-import z from 'zod';
-
-type RefreshTknUserCookieType = z.infer<typeof refreshTknUserCookieSchema>;
-const refreshTknUserCookieSchema = z.object({
-  token: z.string().min(1)
-});
-
-export { refreshTknUserCookieSchema, RefreshTknUserCookieType };
