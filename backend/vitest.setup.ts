35c7a33a5d548750e16427f992fa1a5995cbe148
Renan Alves <renanlvsdv@gmail.com>
2026-06-06 09:59:47 -0300

    refactor(vitest): atualizar configuração de mock de testes unitários para envs Resend

--- backend/vitest.setup.ts
+++ backend/vitest.setup.ts
@@ -9,12 +9,12 @@ vi.mock('./src/config/env.js', () => ({
     TOKEN_REFRESH_EXPIRES_IN: '7d',
     COOKIE_SECRET: 'mock-cookie-secret',
     AWS_S3_REGION: 'us-east-1',
-    AWS_SES_REGION: 'us-east-1',
     AWS_ACCESS_KEY_ID: 'mock-access-key',
     AWS_SECRET_ACCESS_KEY: 'mock-secret-key',
     AWS_S3_BUCKET: 'mock-bucket',
     AWS_S3_BUCKET_URL: 'http://mock-s3.com',
-    AWS_SES_EMAIL_FROM: 'noreply@mock.com',
-    MAX_DEVICES_PER_USER: 5
+    MAX_DEVICES_PER_USER: 5,
+    RESEND_API_KEY: 'API_KEY_MOCK',
+    SEND_EMAIL_FROM: 'mock@gmail.com'
   }
 }));
